68c7a24cd9ff17cef8772c06794b485715ee53b0
Erin Belot <erinbelot@Erins-MacBook-Pro.local>
2018-07-02 15:52:25 +1000

    initial repo

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+import bb from 'express-busboy';
+
+import todoRoutes from './routes/todo.server.route';
+
+const app = express();
+
+bb.extend(app);
+
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Acept");
+    next();  
+})
+
